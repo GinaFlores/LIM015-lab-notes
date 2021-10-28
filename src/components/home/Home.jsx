ff5efbbf90ee05a1af7b244714ea0d72c90e8857
Gina <floresgina2020@gmail.com>
2021-10-28 11:24:23 -0500

    Header de la vista home e instalación de eslint

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import logoHome from '../../assets/logoNotes.png'
+import signOut from '../../assets/sign-out-alt-solid.svg'
+import '../home/home.css'
 
 function Home () {
     return(
         <section>
+            <header >
+                <div className="headerLogo">
+                    <img className="logoHome" src={logoHome} alt="Notes" />
+                </div>
+                <div className="headerInput">
+                    <input className="inputSeach iconSeach" type="text" placeholder="Busca lo que quieras"/>
+                </div>
+                <div className="headerSignOut">
+                    <img className="signOut" src={signOut} alt="Notes" />
+                </div>
+            </header>
+
+            <hr style={{
+                color: '#616161',
+            }}/>
+
             <h1>Home</h1>
         </section>
     );
